79740ed7560b448e347e47a392608e0e779e7c98
iampenkin@gmail.com <54068102+penkin-repo@users.noreply.github.com>
2025-02-28 01:24:04 +0300

    upload v0.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 import typograf from "astro-typograf";
 
+// https://penkin-repo.github.io/fortune/  
+
 export default defineConfig({
+  site: 'https://penkin-repo.github.io/fortune/ ',
+  base: '/fortune/', 
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
